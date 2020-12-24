6e0fde38d641a7062a5f8f210044cba9dc5e9fcf
Kota Matsumoto <k.matsumoto@blue-corporation.jp>
2020-12-24 21:56:43 +0900

    vue.jsの読み込みと各主値の割当て

--- js/script.js
+++ js/script.js
@@ -2,21 +2,21 @@ const store = new Vuex.Store({
         state: {
             // ボタンの文字を定義
             numbers: [
-                ["", "", "", ""],
-                ["", "", "", ""],
-                ["", "", "", ""],
-                ["", ""]
+                ["7", "8", "9", "C"],
+                ["4", "5", "6", "+"],
+                ["1", "2", "3", "-"],
+                ["0", "="]
             ],
             // 計算結果を設定する変数を定義
-            display:
+            display: "0"
         }
     })
     // mapstateを利用するために記述
-const {} = Vuex
+const { mapState } = Vuex
 
 new Vue({
     store,
     el: "#app",
     // mapStateから値を取得
-
+    computed: mapState(['numbers', 'display'])
 })
