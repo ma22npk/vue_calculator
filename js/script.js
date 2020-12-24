046e5decc640682a38ce83da2573e13ae50c3522
Kota Matsumoto <k.matsumoto@blue-corporation.jp>
2020-12-24 21:45:31 +0900

    最初のコミット、ボタンの作成

---
+++ js/script.js
@@ -0,0 +1,22 @@
+const store = new Vuex.Store({
+        state: {
+            // ボタンの文字を定義
+            numbers: [
+                ["", "", "", ""],
+                ["", "", "", ""],
+                ["", "", "", ""],
+                ["", ""]
+            ],
+            // 計算結果を設定する変数を定義
+            display:
+        }
+    })
+    // mapstateを利用するために記述
+const {} = Vuex
+
+new Vue({
+    store,
+    el: "#app",
+    // mapStateから値を取得
+
+})
